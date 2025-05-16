69c3bd2b71123011488c2a7b5038f4cf9560b5a0
Jamshed <jmustafa@calrom.com>
2025-05-16 14:58:57 +0500

    added caching

--- src/Application/Queries/GetUserByIdQuery.cs
+++ src/Application/Queries/GetUserByIdQuery.cs
@@ -2,38 +2,27 @@
 using AutoMapper;
 using Domain.Interfaces;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Application.Queries
+namespace Application.Queries;
+
+public record GetUserByIdQuery(Guid Id) : IRequest<UserDto>;
+
+public class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery, UserDto>
 {
-    public record GetUserByIdQuery(Guid Id) : IRequest<UserDto>;
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
 
-    public class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery, UserDto>
+    public GetUserByIdQueryHandler(IUserRepository userRepository, IMapper mapper)
     {
-        private readonly IUserRepository _userRepository;
-        private readonly IMapper _mapper;
-        
-        public GetUserByIdQueryHandler(IUserRepository userRepository, IMapper mapper)
-        {
-            _userRepository = userRepository;
-            _mapper = mapper;
-        }
-
-        public async Task<UserDto> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
-        {
-            var user = await _userRepository.GetByIdAsync(request.Id, cancellationToken);
-            
-            if (user == null)
-            {
-                throw new KeyNotFoundException($"User with ID {request.Id} not found");
-            }
-
-            return _mapper.Map<UserDto>(user);
-        }
+        _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
     }
-    
+
+    public async Task<UserDto> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.GetByIdAsync(request.Id, cancellationToken)
+                   ?? throw new KeyNotFoundException($"User with ID {request.Id} not found");
+
+        return _mapper.Map<UserDto>(user);
+    }
 }
--- src/Presentation/WebApi/Controllers/UserController.cs
+++ src/Presentation/WebApi/Controllers/UserController.cs
@@ -3,8 +3,10 @@ using Application.Models;
 using Application.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Primitives;
+using System;
 
 namespace WebApi.Controllers
 {
@@ -14,26 +16,46 @@ namespace WebApi.Controllers
     public class UserController : ControllerBase
     {
         private readonly IMediator _mediator;
+        private readonly IMemoryCache _cache;
+        private static CancellationTokenSource _evictionTokenSource = new();
 
-        public UserController(IMediator mediator)
+        public UserController(IMediator mediator, IMemoryCache cache)
         {
             _mediator = mediator;
+            _cache = cache;
         }
 
-        [HttpGet("{id}")]
+        // GET api/user/{id}
+        [HttpGet("{id:guid}")]
         public async Task<IActionResult> GetUserById(Guid id)
         {
-            var query = new GetUserByIdQuery(id);
-            var result = await _mediator.Send(query);
+            string cacheKey = $"user_{id}";
+
+            var result = await _cache.GetOrCreateAsync(cacheKey, async entry =>
+            {
+                entry.SlidingExpiration = TimeSpan.FromMinutes(5);
+                entry.AddExpirationToken(new CancellationChangeToken(_evictionTokenSource.Token));
+                return await _mediator.Send(new GetUserByIdQuery(id));
+            });
+
             return Ok(result);
         }
 
+        // POST api/user
         [HttpPost]
         //[Authorize(Roles = "Admin")]
-        public async Task<ActionResult<Guid>> CreateUser(CreateUserCommand command)
+        public async Task<ActionResult<Guid>> CreateUser([FromBody] CreateUserCommand command)
         {
             var result = await _mediator.Send(command);
+            InvalidateCache();
             return Ok(result);
+        }
+
+        private void InvalidateCache()
+        {
+            _evictionTokenSource.Cancel();
+            _evictionTokenSource.Dispose();
+            _evictionTokenSource = new CancellationTokenSource(); // Renew token for next cache cycle
         }
     }
 }
